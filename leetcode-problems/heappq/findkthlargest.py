8b0b04352150ca15c46e65681841581e5b3524c6
Neil <ebox381@gmail.com>
2026-07-03 20:05:30 -0400

    did 2-3 again

--- leetcode-problems/heappq/findkthlargest.py
+++ leetcode-problems/heappq/findkthlargest.py
@@ -1,3 +1,10 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return heapq.nlargest(k,nums)[-1]
+
+
+
+
+'''
+This is extremely important function for min heaps
+'''
