66c38a456320cd4ddf578c556e67d4968164d533
Neil <ebox381@gmail.com>
2026-07-05 14:59:43 -0400

    backtracking problem 1

---
+++ leetcode-problems/backtracking/combinationsum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        out = []
+        nums.sort()
+
+        def backtrack(index, path):
+            print(path)
+            if sum(path) == target:
+                out.append(path.copy())
+                return
+            for i in range(index,len(nums)):
+                
+                if sum(path) + nums[i] > target:
+                    return
+                path.append(nums[i])
+                backtrack(i,path)
+                path.pop()
+               
+
+        backtrack(0,[])
+        return out
+
+   
